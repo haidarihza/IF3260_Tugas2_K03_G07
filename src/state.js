8592d1fd27f17897ef20261e1ae607a3c9b3a4c4
Aloysius Gilang <gilangpramdya84@gmail.com>
2023-03-25 01:24:31 +0700

    isolate shader, add transformation per obj

--- src/state.js
+++ src/state.js
@@ -1,50 +1,5 @@
 "use strict";
 
-/**
- * @type {string}
- * @description Vertex shader source code.
- */
-const vertex = `
-    attribute vec3 position;
-    attribute vec3 normal;
-
-    uniform mat4 Vmatrix;
-    uniform mat4 Mmatrix;
-    uniform mat4 Nmatrix;
-
-    attribute vec3 color;
-    varying vec3 vLighting;
-    varying vec3 vColor;
-
-    void main(void) {
-        gl_Position = Vmatrix*Mmatrix*vec4(position, 1.);
-        vec3 ambientLight = vec3(0.3, 0.3, 0.3);
-        vec3 directionalLightColor = vec3(1, 1, 1);
-        vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
-        vec4 transformedNormal = Nmatrix*vec4(normal, 1.);
-        
-        float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
-        vLighting = ambientLight + (directionalLightColor * directional);
-        vColor = color;
-    }
-`;
-
-/**
- * @type {string}
- * @description Fragment shader source code.
- * */
-// Fragment shader program
-const fragment = `
-    precision mediump float;
-    varying vec3 vColor;
-    varying vec3 vLighting;
-                
-    void main(void) {
-        gl_FragColor = vec4(vColor, 1.);
-        gl_FragColor.rgb *= vLighting;
-    }
-`;
-
 // HTML ELEMENTS.
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("canvas");
@@ -66,64 +21,68 @@ var fileInput = document.getElementById("fileinput");
 var saveButton = document.getElementById("save-button");
 var selector = document.getElementById("object-option");
 var projectionViewVal = 1;
-var translation = [0, 0, 0];
-var rotation = [degToRad(0), degToRad(0), degToRad(0)];
-var scale = [1, 1, 1];
-var camera = [0, 0];
 
-rotateX.addEventListener("input", function (e) {
-  rotation[0] = degToRad(e.target.value);
-  document.getElementById("rotate-x-value").innerHTML = e.target.value;
-  drawObject();
-});
+function addEventListeners() {
+  console.log("selectedIdx ->", selectedIdx);
+  var camera = [0, 0];
 
-rotateY.addEventListener("input", function (e) {
-  rotation[1] = degToRad(e.target.value);
-  document.getElementById("rotate-y-value").innerHTML = e.target.value;
-  drawObject();
-});
+  rotateX.addEventListener("input", function (e) {
+    objects[selectedIdx].rotation[0] = degToRad(e.target.value);
+    document.getElementById("rotate-x-value").innerHTML = e.target.value;
+    for (var i = 0; i < objects.length; i++) {
+      console.log("idx", i, "rotation", objects[i].rotation);
+    }
+    drawObject();
+  });
 
-rotateZ.addEventListener("input", function (e) {
-  rotation[2] = degToRad(e.target.value);
-  document.getElementById("rotate-z-value").innerHTML = e.target.value;
-  drawObject();
-});
+  rotateY.addEventListener("input", function (e) {
+    objects[selectedIdx].rotation[1] = degToRad(e.target.value);
+    document.getElementById("rotate-y-value").innerHTML = e.target.value;
+    drawObject();
+  });
 
-translationX.addEventListener("input", function (e) {
-  translation[0] = e.target.value;
-  document.getElementById("translate-x-value").innerHTML = e.target.value;
-  drawObject();
-});
+  rotateZ.addEventListener("input", function (e) {
+    objects[selectedIdx].rotation[2] = degToRad(e.target.value);
+    document.getElementById("rotate-z-value").innerHTML = e.target.value;
+    drawObject();
+  });
 
-translationY.addEventListener("input", function (e) {
-  translation[1] = e.target.value;
-  document.getElementById("translate-y-value").innerHTML = e.target.value;
-  drawObject();
-});
+  translationX.addEventListener("input", function (e) {
+    objects[selectedIdx].translation[0] = e.target.value;
+    document.getElementById("translate-x-value").innerHTML = e.target.value;
+    drawObject();
+  });
 
-translationZ.addEventListener("input", function (e) {
-  translation[2] = e.target.value;
-  document.getElementById("translate-z-value").innerHTML = e.target.value;
-  drawObject();
-});
+  translationY.addEventListener("input", function (e) {
+    objects[selectedIdx].translation[1] = e.target.value;
+    document.getElementById("translate-y-value").innerHTML = e.target.value;
+    drawObject();
+  });
 
-scaleX.addEventListener("input", function (e) {
-  scale[0] = e.target.value;
-  document.getElementById("scale-x-value").innerHTML = e.target.value;
-  drawObject();
-});
+  translationZ.addEventListener("input", function (e) {
+    objects[selectedIdx].translation[2] = e.target.value;
+    document.getElementById("translate-z-value").innerHTML = e.target.value;
+    drawObject();
+  });
 
-scaleY.addEventListener("input", function (e) {
-  scale[1] = e.target.value;
-  document.getElementById("scale-y-value").innerHTML = e.target.value;
-  drawObject();
-});
+  scaleX.addEventListener("input", function (e) {
+    objects[selectedIdx].scale[0] = e.target.value;
+    document.getElementById("scale-x-value").innerHTML = e.target.value;
+    drawObject();
+  });
 
-scaleZ.addEventListener("input", function (e) {
-  scale[2] = e.target.value;
-  document.getElementById("scale-z-value").innerHTML = e.target.value;
-  drawObject();
-});
+  scaleY.addEventListener("input", function (e) {
+    objects[selectedIdx].scale[1] = e.target.value;
+    document.getElementById("scale-y-value").innerHTML = e.target.value;
+    drawObject();
+  });
+
+  scaleZ.addEventListener("input", function (e) {
+    objects[selectedIdx].scale[2] = e.target.value;
+    document.getElementById("scale-z-value").innerHTML = e.target.value;
+    drawObject();
+  });
+}
 
 // TODO: handle camera
 cameraRotate.addEventListener("input", function (e) {
@@ -158,11 +117,19 @@ fileInput.addEventListener("click", function (e) {
     reader.onload = (e) => {
       const result = e.target.result;
       const data = JSON.parse(result);
+
+      // push default transformation
+      data.forEach((obj) => {
+        obj.translation = [0, 0, 0];
+        obj.rotation = [degToRad(0), degToRad(0), degToRad(0)];
+        obj.scale = [1, 1, 1];
+      });
       objects.push(...data);
-      console.log("loading obj :" + objects);
 
       // draw all objects
       drawObject();
+      // add event listeners after loading objects
+      addEventListeners();
     };
     reader.readAsText(file);
   };
